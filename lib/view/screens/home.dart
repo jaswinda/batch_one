595e7205cdfc3bb9ab39e26f4ef88f7b2156880a
jaswinda <singhjassi2624@gmail.com>
2021-10-28 15:28:42 +0545

    up change

--- lib/view/screens/home.dart
+++ lib/view/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:batch_one/services/authentication.dart';
+import 'package:batch_one/view/screens/products_screen.dart';
 import 'package:batch_one/view/screens/user_auth_checker.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -15,12 +16,7 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   List<Widget> _buildScreens() {
     return [
-      Stack(
-        fit: StackFit.expand,
-        children: [
-          buildFloatingSearchBar(),
-        ],
-      ),
+      ProductScreen(),
       Container(),
       Container(
           child: Column(
@@ -125,55 +121,5 @@ class _HomeState extends State<Home> {
     //     ],
     //   ),
     // );
-  }
-
-  Widget buildFloatingSearchBar() {
-    final isPortrait =
-        MediaQuery.of(context).orientation == Orientation.portrait;
-
-    return FloatingSearchBar(
-      hint: 'Search...',
-      scrollPadding: const EdgeInsets.only(top: 16, bottom: 56),
-      transitionDuration: const Duration(milliseconds: 800),
-      transitionCurve: Curves.easeInOut,
-      physics: const BouncingScrollPhysics(),
-      axisAlignment: isPortrait ? 0.0 : -1.0,
-      openAxisAlignment: 0.0,
-      width: isPortrait ? 600 : 500,
-      debounceDelay: const Duration(milliseconds: 500),
-      onQueryChanged: (query) {
-        // Call your model, bloc, controller here.
-      },
-      // Specify a custom transition to be used for
-      // animating between opened and closed stated.
-      transition: CircularFloatingSearchBarTransition(),
-      actions: [
-        FloatingSearchBarAction(
-          showIfOpened: false,
-          child: CircularButton(
-            icon: const Icon(Icons.place),
-            onPressed: () {},
-          ),
-        ),
-        FloatingSearchBarAction.searchToClear(
-          showIfClosed: false,
-        ),
-      ],
-      builder: (context, transition) {
-        return ClipRRect(
-          borderRadius: BorderRadius.circular(8),
-          child: Material(
-            color: Colors.white,
-            elevation: 4.0,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: Colors.accents.map((color) {
-                return Container(height: 112, color: color);
-              }).toList(),
-            ),
-          ),
-        );
-      },
-    );
   }
 }
